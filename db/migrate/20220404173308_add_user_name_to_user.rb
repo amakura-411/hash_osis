cc5a0ba33cc79b31d2fbf22ee1dd242505c23379
amakura <callplus411@gmail.com>
2022-04-05 03:26:01 +0900

    y

--- db/migrate/20220404173308_add_user_name_to_user.rb
+++ db/migrate/20220404173308_add_user_name_to_user.rb
@@ -1,5 +1,6 @@
 class AddUserNameToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :username, :string, null: false
+    add_column :users, :deleted_at, :datetime
+    add_index :users, :deleted_at
   end
 end
