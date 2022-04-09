bd86ca66f6dd9bd76cee79f6ac594f52aca80409
amakura <callplus411@gmail.com>
2022-04-09 20:38:27 +0900

    アラート　黒くなった

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_04_05_140508) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["character_id"], name: "index_favorites_on_character_id"
     t.index ["user_id", "character_id"], name: "index_favorites_on_user_id_and_character_id", unique: true
+    t.index ["chara_name", "appear_in"],unique: true
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
